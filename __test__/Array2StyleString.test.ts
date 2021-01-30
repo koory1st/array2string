816446b9691bfe91b31911004d9ea01eef4ea505
koory1st <32436334@qq.com>
2021-01-30 15:50:35 +0800

    feat(array2stylestring): add

---
+++ __test__/Array2StyleString.test.ts
@@ -0,0 +1,51 @@
+/** @format */
+import {array2StyleString} from '../src/Array2StyleString'
+
+describe('Test of Array2StyleString', () => {
+  test('[] => null', () => {
+    const input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[] = []
+    expect(array2StyleString(input)).toBeNull()
+  })
+  test('["color:red", "font-size:10px"] => "color:red;font-size:10px;"', () => {
+    const input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[] = [
+      'color:red',
+      'font-size:10px',
+    ]
+    expect(array2StyleString(input)).toBe('color:red;font-size:10px;')
+  })
+  test('["color:red;", "font-size:10px;"] => "color:red;font-size:10px;"', () => {
+    const input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[] = [
+      'color:red;',
+      'font-size:10px;',
+    ]
+    expect(array2StyleString(input)).toBe('color:red;font-size:10px;')
+  })
+  test('[["color", "red"],["font-size", "10px"]] => "color:red;font-size:10px;"', () => {
+    const input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[] = [
+      ['color', 'red'],
+      ['font-size', '10px'],
+    ]
+    expect(array2StyleString(input)).toBe('color:red;font-size:10px;')
+  })
+  test('[["color", "red"],["font-size", 10]] => "color:red;font-size:10px;"', () => {
+    const input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[] = [
+      ['color', 'red'],
+      ['font-size', 10],
+    ]
+    expect(array2StyleString(input)).toBe('color:red;font-size:10px;')
+  })
+  test('[["color", "red", false],["font-size", 10, false]] => null', () => {
+    const input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[] = [
+      ['color', 'red', false],
+      ['font-size', 10, false],
+    ]
+    expect(array2StyleString(input)).toBeNull()
+  })
+  test('[["color", "red", true],["font-size", 10, true]] => "color:red;font-size:10px;"', () => {
+    const input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[] = [
+      ['color', 'red', true],
+      ['font-size', 10, true],
+    ]
+    expect(array2StyleString(input)).toBe('color:red;font-size:10px;')
+  })
+})
---
+++ src/Array2StyleString.ts
@@ -0,0 +1,53 @@
+/** @format */
+
+/**
+ * array2StyleString
+ * [] => null
+ * ["color:red", "font-size:10px"] => "color:red;font-size:10px;"
+ * ["color:red;", "font-size:10px;"] => "color:red;font-size:10px;"
+ * [["color", "red"],["font-size", "10px"]] => "color:red;font-size:10px;"
+ * [["color", "red"],["font-size", 10]] => "color:red;font-size:10px;"
+ * [["color", "red", false],["font-size", 10, false]] => null
+ * [["color", "red", true],["font-size", 10, true]] => "color:red;font-size:10px;"
+ *
+ * @param input
+ */
+export function array2StyleString(
+  input: (string | string[] | [string, number] | [string, string, boolean] | [string, number, boolean])[],
+): string | null {
+  if (!input || input.length === 0) {
+    return null
+  }
+
+  let rt = ''
+  for (const item of input) {
+    // ["color:red", "font-size:10px"]
+    if (typeof item === 'string') {
+      rt += item
+      if (!rt.endsWith(';')) {
+        rt += ';'
+      }
+      continue
+    }
+
+    // [["color", "red", false],["font-size", 10, false]]
+    if (item.length > 2 && !item[2]) {
+      continue
+    }
+
+    const key = item[0]
+    let value = item[1]
+
+    // [["color", "red"],["font-size", 10]]
+    if (typeof value === 'number') {
+      value = value + 'px'
+    }
+    rt += key + ':' + value
+
+    if (!rt.endsWith(';')) {
+      rt += ';'
+    }
+  }
+
+  return rt ? rt : null
+}
